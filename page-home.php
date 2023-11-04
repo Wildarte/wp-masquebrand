f031744a46979d3b0e91c7b4288e462b4237b8b2
wildarte <wildarte10@gmail.com>
2023-11-04 19:21:35 -0300

    atualizacoes

--- page-home.php
+++ page-home.php
@@ -4,7 +4,13 @@
 
 <main>
     <section class="hero_home d-flex">
-        <img class="img_hero" src="<?= get_template_directory_uri() ?>/assets/img/hero.jpeg" alt="">
+        <?php
+            $img_home_top = get_field('imagem_topo');
+
+            if($img_home_top):
+        ?>
+        <img class="img_hero" src="<?= $img_home_top['url'] ?>" alt="">
+        <?php endif; ?>
         <div class="over_content_hero_home">
             <div class="container d-flex content_hero_home">
                 <div class="w-100 d-flex justify-space-between align-items-center">
@@ -34,11 +40,18 @@
                 </div>
             </div>
             <div class="f-50 d-flex right_content justify-flex-end inner_img_fluid">
-                <img src="<?= get_template_directory_uri() ?>/assets/img/imag.jpg" alt="" class="">
+                <?php
+                    $img_second_home = get_field('imagem_topo');
+
+                    if($img_second_home):
+                ?>
+                <img src="<?= $img_second_home['url'] ?>" alt="" class="">
+                <?php endif; ?>
             </div>
         </div>
     </section>
 
+    <!-- 
     <section>
         <div class="container d-flex section-default">
             <div class="f-25 card_count">
@@ -63,13 +76,24 @@
             </div>
         </div>
     </section>
+     -->
 
     <section>
         <div class="container section-default d-flex text-center">
-            <h2 class="title-default m-auto f-70 text-center font-lato title_cta text-uppercase">TODO grande DESIGN COMEÇA COM UMA HISTÓRIA AINDA MELHOR</h2>
+            <?php
+                $cta_text = get_field('cta_text');
+                if($cta_text):
+            ?>
+            <h2 class="title-default m-auto f-70 text-center font-lato title_cta text-uppercase"><?= $cta_text; ?></h2>
+            <?php endif; ?>
         </div>
-        <div class="container section-default">
-            <img src="<?= get_template_directory_uri() ?>/assets/img/imas.jpeg" alt="">
+        <div class="container section-default cta_img">
+            <?php
+                $imagem_cta = get_field('imagem_cta');
+                if($imagem_cta):
+            ?>
+            <img src="<?= $imagem_cta['url'] ?>" alt="">
+            <?php endif; ?>
         </div>
     </section>
 
@@ -90,99 +114,45 @@
                 </div>
             </div>
             <div class="f-50 d-flex right_content justify-flex-end inner_img_fluid">
-                <img src="<?= get_template_directory_uri() ?>/assets/img/imag.jpg" alt="" class="">
+                <?php
+                    $imagem_sessao_inferior = get_field('imagem_sessao_inferior');
+                    if($imagem_sessao_inferior):
+                ?>
+                <img src="<?= $imagem_sessao_inferior['url'] ?>" alt="" class="">
+                <?php endif; ?>
             </div>
         </div>
     </section>
 
-    <section>
-        <div class="container d-flex section-default">
-            <h3 class="w-100 subtitle">Nossos</h3>
-            <h2 class="title-second m-b-60">Últimos Projetos</h2>
-            
-            <section class="d-flex">
-                <div class="f-50">
-                    <article class="card_project">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img//card-project.jpg" alt="">
-                        <a href="#" class="info_card_project color-white">
-                            <h3 class="title-third">Dark and spacious apartment for a family</h3>
-                            <div class="d-flex w-100">
-                                <div class="btn-default w-100 d-flex justify-space-between color-white btn-icon align-items-center btn_card_project">Ver projeto <span><i class="bi bi-arrow-right"></i></span></div>
-                            </div>
-                        </a>
-                    </article>
-                </div>
-                <div class="f-50">
-                    <article class="card_project">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img//card-project.jpg" alt="">
-                        <a href="#" class="info_card_project color-white">
-                            <h3 class="title-third">Dark and spacious apartment for a family</h3>
-                            <div class="d-flex w-100">
-                                <div class="btn-default w-100 d-flex justify-space-between color-white btn-icon align-items-center btn_card_project">Ver projeto <span><i class="bi bi-arrow-right"></i></span></div>
-                            </div>
-                        </a>
-                    </article>
-                </div>
-                <div class="f-50">
-                    <article class="card_project">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img//card-project.jpg" alt="">
-                        <a href="#" class="info_card_project color-white">
-                            <h3 class="title-third">Dark and spacious apartment for a family</h3>
-                            <div class="d-flex w-100">
-                                <div class="btn-default w-100 d-flex justify-space-between color-white btn-icon align-items-center btn_card_project">Ver projeto <span><i class="bi bi-arrow-right"></i></span></div>
-                            </div>
-                        </a>
-                    </article>
-                </div>
-                <div class="f-50">
-                    <article class="card_project">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img//card-project.jpg" alt="">
-                        <a href="#" class="info_card_project color-white">
-                            <h3 class="title-third">Dark and spacious apartment for a family</h3>
-                            <div class="d-flex w-100">
-                                <div class="btn-default w-100 d-flex justify-space-between color-white btn-icon align-items-center btn_card_project">Ver projeto <span><i class="bi bi-arrow-right"></i></span></div>
-                            </div>
-                        </a>
-                    </article>
-                </div>
-            </section>
-
-            <div class="w-100 text-center p-10">
-                <a href="" class="btn-default color-white">Ver mais <span><i class="bi bi-plus-circle"></i></span></a>
-            </div>
-            
-        </div>
-    </section>
+    <?php get_template_part('templates-parts/content', 'list-projetos') ?>
 
     <section>
         <div class="container section-default d-flex">
             <div class="f-50 left_faq">
-                <img src="<?= get_template_directory_uri() ?>/assets/img/imag.jpg" alt="">
+                <img src="<?= get_template_directory_uri() ?>/assets/img/faq.jpg" alt="">
             </div>
             <div class="f-50 right_faq">
 
-                <h2 class="title-second m-b-60">POPULAR QUESTIONS</h2>
+                <h2 class="title-second m-b-60">Perguntas Frequentes</h2>
 
                 <ul class="list_faq">
+                    <?php
+                        
+                        $faq = get_field('faq');
+
+                        //var_dump($faq);
+
+                        foreach($faq as $item):
+
+                    ?>
                     <li>
-                        <div class="item_faq d-flex align-items-center"><span><i class="bi bi-x-circle"></i></span> <h3>WHAT IS INCLUDED IN THE DESIGN PROJECT?</h3></div>
-                        <p>Planning solution, 3D visualization, drawings and detailed specifications with binding materials to the location of the object.</p>
+                        <div class="item_faq d-flex align-items-center"><span><i class="bi bi-x-circle"></i></span> <h3><?= $item['pergunta'] ?></h3></div>
+                        <p><?= $item['resposta'] ?></p>
                     </li>
 
-                    <li>
-                        <div class="item_faq d-flex align-items-center"><span><i class="bi bi-x-circle"></i></span> <h3>WHAT IS INCLUDED IN THE DESIGN PROJECT?</h3></div>
-                        <p>Planning solution, 3D visualization, drawings and detailed specifications with binding materials to the location of the object.</p>
-                    </li>
+                    <?php endforeach; ?>
 
-                    <li>
-                        <div class="item_faq d-flex align-items-center"><span><i class="bi bi-x-circle"></i></span> <h3>WHAT IS INCLUDED IN THE DESIGN PROJECT?</h3></div>
-                        <p>Planning solution, 3D visualization, drawings and detailed specifications with binding materials to the location of the object.</p>
-                    </li>
-
-                    <li>
-                        <div class="item_faq d-flex align-items-center"><span><i class="bi bi-x-circle"></i></span> <h3>WHAT IS INCLUDED IN THE DESIGN PROJECT?</h3></div>
-                        <p>Planning solution, 3D visualization, drawings and detailed specifications with binding materials to the location of the object.</p>
-                    </li>
+                    
                 </ul>
             </div>
         </div>
@@ -208,7 +178,7 @@
                 
             </div>
             <div class="f-50 d-flex right_content justify-flex-end">
-                <img src="<?= get_template_directory_uri() ?>/assets/img/imag.jpg" alt="" class="">
+                <img src="<?= get_template_directory_uri() ?>/assets/img/contact.jpg" alt="" class="">
             </div>
         </div>
     </section>
